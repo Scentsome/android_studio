edb77c585f8e1bd84358be0314d8cae2a0e1b254
chronoer <scentsome@gmail.com>
2013-09-04 19:27:07 +0800

    add new hello view interactive pattern

---
+++ HelloAppProject/HelloApp/src/main/java/com/zencher/helloapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.zencher.helloapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        Button button = (Button) findViewById(R.id.button);
+
+
+        button.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v("click", "clicked");
+                TextView text = (TextView)findViewById(R.id.textView);
+                text.setText("Changed");
+
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
